9b53ab7b712ee7670302e5a997cffeb3060fe896
adam <adam@adam>
2024-11-25 10:52:03 -0300

    feat: ajustado o import

--- day.ts
+++ day.ts
@@ -1,15 +1,13 @@
 #!/usr/bin/env -S deno run --allow-read --allow-write
 
-import { leArquivo, parseDict, parsingDict } from "../../utils/utils.ts";
+import { leArquivo, parseDict, parsingDict } from "../utils.ts";
 
 function solver(input: string, part: number) {
-
+  console.log(input);
 }
 
-
-const entrada = "input.txt";
+const entrada = "test.in";
 const input: string = leArquivo(entrada, import.meta.url);
 
-
-console.log('parte 1:', solver(input, 1));
+console.log("parte 1:", solver(input, 1));
 // console.log('parte 2:', solver(input, 2));
