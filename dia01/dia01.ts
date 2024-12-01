297672850d4cc43c09d0b12dcf88c8914bbe69ea
adam <adam@adam>
2024-12-01 09:52:09 -0300

    feat: dia 1 finalizado **

---
+++ dia01/dia01.ts
@@ -0,0 +1,36 @@
+#!/usr/bin/env -S deno run --allow-read --allow-write
+
+import { leArquivo, parseDict, parsingDict } from "../utils.ts";
+
+function solver(input: string, part: number) {
+  let resultado = 0;
+  const lista1: number[]  = [];
+  const lista2: number[]  = [];
+
+  input.split('\n').forEach((linha) => {
+    const [a, b] = linha.split('   ').map(Number);
+    lista1.push(a);
+    lista2.push(b);
+  });
+  
+  for (let i = 0; i < lista1.length; i++) {
+  resultado += Math.abs(lista1[i] - lista2[i])
+  }
+  console.log(resultado);
+
+  resultado = 0;
+  lista1.forEach((numero) => {
+    resultado += numero * lista2.reduce((acc, valor) => {
+      return acc += valor === numero ? 1 : 0
+    }, 0);
+  });
+  console.log(resultado);
+}
+
+
+
+const entrada = "input.in";
+const input: string = leArquivo(entrada, import.meta.url);
+
+console.log("parte 1:", solver(input, 1));
+// console.log('parte 2:', solver(input, 2));
