e488849870c5ca017bcf6c1c92ba85699533e1cc
Adam <Adam@adam.com>
2024-12-03 05:31:18 -0300

    update: tentando ainda

--- dia02/dia02.ts
+++ dia02/dia02.ts
@@ -16,18 +16,48 @@ function solver(input: string, part: number) {
       // ascendente
       let flag = 0;
       for (let i = 0; i < listaLinha.length; i++) {
-        if (listaLinha[i-1] < listaLinha[i]) {
-          continue;
-        } else if (listaLinha[i-1] )  {
-
-          flag += 1
+        if (flag === 2) {
+          break
+        }
+        if (i === 0) {
+          continue
+        } else if (listaLinha[i-1] < listaLinha[i] && Math.abs(listaLinha[i - 1] - listaLinha[i]) < 4) {
+          continue
+        } else if (i === listaLinha.length - 1) {
+          flag += 1;
+          continue
+        } else if (listaLinha[i-1] < listaLinha[i + 1] && Math.abs(listaLinha[i - 1] - listaLinha[i + 1]) < 4) {
+          flag += 1          
+        } else {
+          flag += 2
         }
 
+      }
+      if (flag <= 1) {
+        result += 1;
+      }
+      flag = 0
+      for (let i = 0; i < listaLinha.length; i++) {
+        if (flag === 2) {
+          break
+        }
+        if (i === 0) {
+          continue
+        } else if (listaLinha[i-1] > listaLinha[i] && Math.abs(listaLinha[i - 1] - listaLinha[i]) < 4) {
+          continue
+        } else if (i === listaLinha.length - 1) {
+          flag += 1;
+        } else if (listaLinha[i-1] > listaLinha[i + 1] && Math.abs(listaLinha[i - 1] - listaLinha[i + 1]) < 4) {
+          flag += 1          
+        } else {
+          flag += 2
+        }
 
-        console.log(i)
+      }
+      if (flag <= 1) {
+        result += 1;
       }
     }
-    
   });
   return result;
 }
@@ -36,4 +66,4 @@ const entrada = "input.in";
 const input: string = leArquivo(entrada, import.meta.url);
 
 console.log("parte 1:", solver(input, 1));
-// console.log('parte 2:', solver(input, 2));
+console.log('parte 2:', solver(input, 2));
