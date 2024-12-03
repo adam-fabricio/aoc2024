ec9eb6bba6123430c99773c88a13f9982f91c79d
adam <adam@adam>
2024-12-03 19:19:56 -0300

    feat: formatado dia 1

--- dia01/dia01.ts
+++ dia01/dia01.ts
@@ -4,33 +4,34 @@ import { leArquivo, parseDict, parsingDict } from "../utils.ts";
 
 function solver(input: string, part: number) {
   let resultado = 0;
-  const lista1: number[]  = [];
-  const lista2: number[]  = [];
+  const lista1: number[] = [];
+  const lista2: number[] = [];
 
-  input.split('\n').forEach((linha) => {
-    const [a, b] = linha.split('   ').map(Number);
+  input.split("\n").forEach((linha) => {
+    const [a, b] = linha.split("   ").map(Number);
     lista1.push(a);
     lista2.push(b);
   });
-  
+
   for (let i = 0; i < lista1.length; i++) {
-  resultado += Math.abs(lista1[i] - lista2[i])
+    resultado += Math.abs(lista1[i] - lista2[i]);
   }
-  console.log(resultado);
+  if (part === 1) {
+    return resultado;
+  }
 
   resultado = 0;
   lista1.forEach((numero) => {
     resultado += numero * lista2.reduce((acc, valor) => {
-      return acc += valor === numero ? 1 : 0
+      return acc += valor === numero ? 1 : 0;
     }, 0);
   });
-  console.log(resultado);
+
+  if (part === 2) return resultado;
 }
-
-
 
 const entrada = "input.in";
 const input: string = leArquivo(entrada, import.meta.url);
 
 console.log("parte 1:", solver(input, 1));
-// console.log('parte 2:', solver(input, 2));
+console.log("parte 2:", solver(input, 2));
