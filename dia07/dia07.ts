975593e9e8ebc20f96e48d8d372435e25248e2e1
Adam <Adam@adam.com>
2024-12-12 02:41:54 -0300

    update: dia 8 duas estrelas

--- dia07/dia07.ts
+++ dia07/dia07.ts
@@ -2,27 +2,25 @@
 
 import { leArquivo, parseDict, parsingDict } from "../utils.ts";
 
-
 function geraResultados(listaValores: number[], part: number): number[] {
   const valor = listaValores.pop() ?? 0;
   if (listaValores.length === 0) {
     return [valor];
   }
-  const valores = geraResultados(listaValores, part)
-  const resultados: number[] = []
+  const valores = geraResultados(listaValores, part);
+  const resultados: number[] = [];
   valores.forEach((val) => {
     resultados.push(val + valor);
     resultados.push(val * valor);
     if (part === 2) {
-      resultados.push(Number(val.toString() + valor.toString()))
+      resultados.push(Number(val.toString() + valor.toString()));
     }
   });
   return resultados;
 }
 
 function solver(input: string, part: number) {
-  
-  let result = 0
+  let result = 0;
 
   input.split("\n").forEach((linha) => {
     const [rawResultado, valores] = linha.split(":");
@@ -31,16 +29,15 @@ function solver(input: string, part: number) {
     const resultado = Number(rawResultado);
 
     if (resultados.includes(resultado)) {
-      result += resultado
+      result += resultado;
     }
   });
 
-  return result
-
+  return result;
 }
 
 const entrada = "input.in";
 const input: string = leArquivo(entrada, import.meta.url);
 
 console.log("parte 1:", solver(input, 1));
-console.log('parte 2:', solver(input, 2));
+console.log("parte 2:", solver(input, 2));
--- dia08/dia08.ts
+++ dia08/dia08.ts
@@ -5,26 +5,66 @@ import { leArquivo, parseDict, parsingDict } from "../utils.ts";
 function solver(input: string, part: number) {
   const antenas = new Map<string, [number, number][]>();
   const linhas = input.split("\n");
+
+  const linhasMax = linhas.length;
+  const colunasMax = linhas[0].length;
+
   linhas.forEach((linha, l) => {
     linha.split("").forEach((char, c) => {
       if (char !== ".") {
-        if(antenas.has(char)) {
+        if (antenas.has(char)) {
           const cordenadas = antenas.get(char) ?? [];
-          cordenadas.push([l, c])
-          antenas.set(char, cordenadas)
+          cordenadas.push([l, c]);
+          antenas.set(char, cordenadas);
         } else {
-          antenas.set(char, [[l, c]])
+          antenas.set(char, [[l, c]]);
         }
       }
     });
   });
-  console.log(antenas)
-  
 
+  const antinodes = new Set<string>();
+  for (const antena of antenas.values()) {
+    antena.forEach(([a1l, a1c], a1) => {
+      antena.forEach(([a2l, a2c], a2) => {
+        if (a1 === a2) return;
+
+        const deltaLinha = a1l - a2l;
+        const deltaColuna = a1c - a2c;
+
+        if (part === 1) {
+          const antinodeLinha = a1l + deltaLinha;
+          const antinodeColuna = a1c + deltaColuna;
+
+          if (
+            antinodeLinha < 0 || antinodeLinha >= linhasMax ||
+            antinodeColuna < 0 || antinodeColuna >= colunasMax
+          ) {
+            return;
+          }
+          antinodes.add(`${a1l + deltaLinha},${a1c + deltaColuna}`);
+        } else {
+          let antinodeLinha = a1l + deltaLinha;
+          let antinodeColuna = a1c + deltaColuna;
+          antinodes.add(`${a1l},${a1c}`);
+
+          while (
+            antinodeLinha >= 0 && antinodeLinha < linhasMax &&
+            antinodeColuna >= 0 && antinodeColuna < colunasMax
+          ) {
+            antinodes.add(`${antinodeLinha},${antinodeColuna}`);
+            antinodeLinha += deltaLinha;
+            antinodeColuna += deltaColuna;
+          }
+        }
+      });
+    });
+  }
+  return antinodes.size;
 }
 
 const entrada = "input.in";
 const input: string = leArquivo(entrada, import.meta.url);
 
 console.log("parte 1:", solver(input, 1));
-// console.log('parte 2:', solver(input, 2));
+console.log("parte 2:", solver(input, 2));
